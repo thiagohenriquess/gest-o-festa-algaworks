1541d8c70abea801fd7bfd9cba4e7fedf88e18d3
Thiago Santos <thiago.santos@m4u.com.br>
2016-10-24 14:37:34 -0200

    Tentando adicionar o Total - Aprimorando form

--- src/main/java/com/algaworks/festa/repository/Convidados.java
+++ src/main/java/com/algaworks/festa/repository/Convidados.java
@@ -11,12 +11,14 @@ import com.algaworks.festa.model.Convidado;
 @Repository	
 public class Convidados {
 	
+	private Integer total = 0;
+	
 	private static final List<Convidado> LISTA_CONVIDADOS = new ArrayList<>();
 	
 	static{
 		LISTA_CONVIDADOS.add(new Convidado(	"Fulano",5));
 		LISTA_CONVIDADOS.add(new Convidado(	"Joãozinho",0));
-		LISTA_CONVIDADOS.add(new Convidado(	"Marininha",2));
+		LISTA_CONVIDADOS.add(new Convidado(	"Marininha",2));		
 	}
 	
 	public List<Convidado> todos(){
@@ -25,6 +27,17 @@ public class Convidados {
 	
 	public void adicionar(Convidado convidado){
 		Convidados.LISTA_CONVIDADOS.add(convidado);
+		total +=convidado.getQuantidadeAcompanhantes();
+	}
+	
+	public void contaTotal(){
+		total =0;
+		for(int i=0;i<LISTA_CONVIDADOS.size();i++){
+			this.total = LISTA_CONVIDADOS.get(i).getQuantidadeAcompanhantes();
+		} 		
+	}
+	public Integer getTotal(){
+		return total;
 	}
 
 }
